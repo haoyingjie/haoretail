98afd0c1be55b883c5bcd8b00bbbd199662f0d75
haoyingjie <haoyingjie@163.com>
2018-07-20 10:36:43 +0800

    新添加了一个User类

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -5,6 +5,14 @@ public class HelloGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello git this is my first 。java！");
+		
+		User zhangsan=new User();
+		zhangsan.setAge(23);
+		zhangsan.setName("张宝宝");
+		
+		System.out.println(zhangsan.getName()+"年龄是："+zhangsan.getAge()+"说"+zhangsan.speak("张楚岚，你干啥子！"));
+		
 	}
+	
 
 }
